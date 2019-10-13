0ae831e8eeb84842cfe596f8bf309da9011e09c1
Brad Smith <bbbradsmith@users.noreply.github.com>
2019-10-13 00:59:28 -0400

    filename should be LOUDER

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 @del temp\*.s
 @del temp\*.o
-@del temp\khan.nes
+@del temp\KHAN.NES
 @del temp\khan.dbg
 @del temp\khan.map
 
 cc65\bin\ca65 -o temp\khan.o -g khan.s
 @IF ERRORLEVEL 1 GOTO error
 
-cc65\bin\ld65 -o temp\khan.nes -m temp\khan.map --dbgfile temp\khan.dbg -C nrom128.cfg temp\khan.o
+cc65\bin\ld65 -o temp\KHAN.NES -m temp\khan.map --dbgfile temp\khan.dbg -C nrom128.cfg temp\khan.o
 @IF ERRORLEVEL 1 GOTO error
 
 @echo.
